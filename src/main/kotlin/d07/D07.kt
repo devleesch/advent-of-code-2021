3a59e80d64db357f3c0a2614dae840e78414e7c8
De Vleeschauwer Xavier <devleesch@gmail.com>
2021-12-07 07:52:44 +0100

    Day 07

---
+++ src/main/kotlin/d07/D07.kt
@@ -0,0 +1,58 @@
+package d07
+
+import readLines
+import java.lang.Integer.min
+import java.lang.Math.abs
+
+fun main() {
+    println("== Day 07 ==")
+    val lines = readLines("src/main/resources/d07/input.txt", String::class)
+
+    println("part 1: " + part1(lines))
+    println("part 2: " + part2(lines))
+
+    println("============")
+}
+
+fun part1(lines: List<String>): Any? {
+    val positions = lines[0].split(',')
+        .map { s -> s.toInt() }
+        .sorted()
+
+    var minFuel = Int.MAX_VALUE
+    for (i in 0..positions.maxOf { p -> p }) {
+        var fuel = 0
+        for (p in positions) {
+            fuel += abs(i - p)
+        }
+        minFuel = min(minFuel, fuel)
+    }
+
+    return minFuel
+}
+
+fun part2(lines: List<String>): Any? {
+    val positions = lines[0].split(',')
+        .map { s -> s.toInt() }
+        .sorted()
+
+    var minFuel = Int.MAX_VALUE
+    for (i in 0..positions.maxOf { p -> p }) {
+        var fuel = 0
+        for (p in positions) {
+            fuel += fuel(abs(i - p))
+        }
+        minFuel = min(minFuel, fuel)
+    }
+
+    return minFuel
+}
+
+fun fuel(move: Int): Int {
+    var fuel = 0
+    for (i in 0..move) {
+        fuel += i
+    }
+    return fuel
+}
+
