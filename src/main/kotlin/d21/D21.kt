14af581e17fed50f25ce61607bcb9e941e7e5b6e
Xavier De Vleeschauwer <devleesch@gmail.com>
2021-12-21 17:37:33 +0100

    Day 21.1

---
+++ src/main/kotlin/d21/D21.kt
@@ -0,0 +1,94 @@
+package d21
+
+import readLines
+import java.lang.Integer.min
+
+fun main() {
+    val day = "21"
+    println("== Day $day ==")
+    val lines = readLines("src/main/resources/d$day/input.txt", String::class)
+
+    println("part 1: " + part1(lines))
+    println("part 2: " + part2(lines))
+
+    println("============")
+}
+
+fun part1(lines: List<String>): Any? {
+    val player1 = Player(lines[0].last().digitToInt(), 0)
+    val player2 = Player(lines[1].last().digitToInt(), 0)
+
+    val dice = Dice(100)
+    while(true) {
+        player1.move(dice)
+//        println("player1: ${player1.points}")
+        if (player1.points >= 1000) break
+
+        player2.move(dice)
+//        println("player2: ${player2.points}")
+        if (player2.points >= 1000) break
+    }
+
+    return min(player1.points, player2.points) * dice.turn * 3
+}
+
+fun part2(lines: List<String>): Any? {
+    val player1 = Player(lines[0].last().digitToInt(), 0)
+    val player2 = Player(lines[1].last().digitToInt(), 0)
+
+    val score = 21
+
+    val dice = DiracDice()
+    while(true) {
+        player1.move(dice)
+//        println("player1: ${player1.points}")
+        if (player1.points >= score) break
+
+        player2.move(dice)
+//        println("player2: ${player2.points}")
+        if (player2.points >= score) break
+    }
+
+    return min(player1.points, player2.points) * dice.turn * 3
+}
+
+class Player(var position: Int, var points: Int) {
+
+    fun move(dice: Dice) {
+        for (roll in dice.roll()) {
+            position += roll
+            while (position > 10) position -= 10
+        }
+        points += position
+    }
+
+}
+
+open class Dice(val side: Int) {
+
+    var turn = 0
+
+    open fun roll(): List<Int> {
+        val roll = mutableListOf<Int>()
+
+        for (i in 1..3) {
+            var value = (turn * 3 + i)
+            if (value > side) value %= side
+            roll.add(value)
+        }
+
+        turn++
+
+        return roll
+    }
+
+}
+
+class DiracDice() : Dice(3) {
+    override fun roll(): List<Int> {
+        turn++
+        return listOf(3, 3, 3)
+    }
+}
+
+
